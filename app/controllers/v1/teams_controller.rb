ed9c37457238e31da4e54078d0f74084ced197c4
Paul McCarthy <phantasos42@gmail.com>
2018-04-24 21:42:04 -0500

    as json and associations

--- app/controllers/v1/teams_controller.rb
+++ app/controllers/v1/teams_controller.rb
@@ -2,4 +2,6 @@ class V1::TeamsController < ApplicationController
   def index
     render json: Team.all.as_json
   end
+
+  
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,17 @@
 class Team < ApplicationRecord
+  belongs_to :division
+
+  def league
+    League.find_by(id: division.league_id)
+  end
+
+  def as_json
+    {
+      city: city,
+      name: name,
+      abbrev: abbrev,
+      league: league.name,
+      division: division.name
+    }
+  end
 end
